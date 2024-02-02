a980cd8e811dcafa59bc0a34fa855079081799db
bachan50 <shiv.bachan50@gmail.com>
2024-02-02 20:53:08 +0530

    Added Exception handling package

---
+++ Oops_Practice/Oops1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Practice
+{
+    internal class Oops1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learning OOPS Concept");
+        }
+    }
+}
