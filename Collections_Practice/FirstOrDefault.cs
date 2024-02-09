92e396d0452c8805807d2ec386fb4baa0fdc7e94
bachan50 <shiv.bachan50@gmail.com>
2024-02-09 11:05:35 +0530

    push all

---
+++ Collections_Practice/FirstOrDefault.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Practice
+{
+    public class FirstOrDefault
+    {
+        public static void main(string[] args) {
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            int firstEvenNumber = numbers.FirstOrDefault(x => x % 2 == 0);
+
+            Console.WriteLine(firstEvenNumber); // Output: 2
+
+        }
+    }
+}
